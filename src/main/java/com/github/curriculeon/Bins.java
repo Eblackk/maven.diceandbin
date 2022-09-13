69e095484598b9030280edfd9fb8e8c504bcb8f1
Eblackk <aps14blacerig@gmail.com>
2022-09-12 21:08:24 -0400

    Passed GetBinTest Test

--- src/main/java/com/github/curriculeon/Bins.java
+++ src/main/java/com/github/curriculeon/Bins.java
@@ -1,16 +1,51 @@
 package com.github.curriculeon;
 
 public class Bins {
+    Integer minFaceValue;
+    Integer maxFaceValue;
+    Bin[] binArray;
+
     public Bins() {
+        this(1,6);
+
     }
 
     public Bins(Integer minFaceValue, Integer maxFaceValue) {
+        this.minFaceValue = minFaceValue;
+        this.maxFaceValue = maxFaceValue;
+        int numberOfBins = maxFaceValue - minFaceValue;
+        this.binArray = new Bin[numberOfBins];
+        int currentLoopNumber = 0;
+        while (currentLoopNumber < numberOfBins) {
+            int currentFaceValue = this.minFaceValue + currentLoopNumber;
+            Bin newBin = new Bin(currentFaceValue);
+            binArray[currentLoopNumber] = newBin;
+            currentLoopNumber = currentLoopNumber + 1;
+        }
+
     }
+
 
     public void incrementBin(Integer faceValueToIncrement) {
     }
 
     public Bin getBin(Integer faceValueOfBin) {
+        int currentLoopNumber = 0;
+        int lastLoopNumber = binArray.length;
+        while (currentLoopNumber < lastLoopNumber ) {
+            Bin currentBin = this.binArray[currentLoopNumber];
+            int faceValue = currentBin.getFaceValueToTrack();
+            if (faceValue == faceValueOfBin) {
+                return currentBin;
+            }
+
+
+            currentLoopNumber = currentLoopNumber + 1;
+
+        }
+
+
+
         return null;
     }
 
