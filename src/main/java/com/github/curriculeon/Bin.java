9a3e4413dffe7b7b46f83e788be491589871d69a
Eblackk <aps14blacerig@gmail.com>
2022-09-12 20:00:42 -0400

    Passed Bin Constructor Test

--- src/main/java/com/github/curriculeon/Bin.java
+++ src/main/java/com/github/curriculeon/Bin.java
@@ -1,20 +1,29 @@
 package com.github.curriculeon;
 
 public class Bin {
+    Integer faceValueToTrack;
+    Integer numberOfOccurences;
     public Bin(Integer faceValueToTrack) {
+        this. faceValueToTrack = faceValueToTrack;
+        this.numberOfOccurences = 0;
+
     }
 
     Bin(Integer faceValueToTrack, Integer numberOfOccurrences) {
+        this.faceValueToTrack = faceValueToTrack;
+        this. numberOfOccurences = numberOfOccurrences;
     }
 
     public Integer getFaceValueToTrack() {
-        return null;
+        return this.faceValueToTrack;
     }
 
     public Integer getNumberOfOccurrences() {
-        return null;
+
+        return this.numberOfOccurences;
     }
 
     public void increment() {
+        numberOfOccurences = numberOfOccurences + 1;
     }
 }
